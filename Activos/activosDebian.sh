a7a977c5c40d5a423b43105362776be57e705513
JaviMQ28 <javier_manceraq@ciecias.unam.mx>
2025-04-23 01:36:17 -0600

    Verifica las computadoras que estan activas

--- Activos/activosDebian.sh
+++ Activos/activosDebian.sh
@@ -1,40 +1,78 @@
-# !/bin/bash
+#!/bin/bash
 
+# Recuperar datos del csv
 # Computadoras
-computers=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1.csv | perl -pe ' s{ (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$) }{ $& eq "," ? ";" : $& }egx ' | awk -F ',' '{print $4}' | awk 'BEGIN{RS=""}{gsub(/\n/, " "); print}')
-
-echo $computers
-
-# IPsV4
-ip4s=$( tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1.csv | perl -pe '
-  s{
-    (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$)  # Detecta comas
-
-  }{
-    $& eq "," ? ";" : $&
-  }egx
-' | awk -F ',' '{print $5}' | awk 'BEGIN{RS=""}{gsub(/\n/, " "); print}')
-
-echo $ip4s
-
-# IPsV6
-ip6s=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1.csv | perl -pe '
+computers=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1\ -\ MODIFICADO.csv| perl -pe '
   s{
     (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$) 
   }{
     $& eq "," ? ";" : $&
   }egx
-' | awk -F ',' '{print $6}' | awk 'BEGIN{RS=""}{gsub(/\n/, " "); print}')
+' | awk -F ';' '{print($4)}' | sort -u)
 
-echo $ip6s
+echo $computers
 
-# Usuarios -> No recupera los que tienen un salto de linea
-users=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1.csv | perl -pe '
+# IPsV4
+ip4s=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1\ -\ MODIFICADO.csv| perl -pe '
   s{
-    (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$)  # detecta comas fuera de comillas
+    (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$) 
   }{
     $& eq "," ? ";" : $&
   }egx
-' | awk -F ',' '{print $7}' | awk 'BEGIN{RS=""}{gsub(/\n/, " "); print}')
+' | awk -F ';' '{print($5)}' | sort -u)
+
+echo $ip4s
+
+# IPsV6
+ip6s=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1\ -\ MODIFICADO.csv| perl -pe '
+  s{
+    (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$) 
+  }{
+    $& eq "," ? ";" : $&
+  }egx
+' | awk -F ';' '{print($6)}' | sort -u)
+
+echo $ip6s
+
+# Usuarios
+users=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1\ -\ MODIFICADO.csv| perl -pe '
+  s{
+    (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$) 
+  }{
+    $& eq "," ? ";" : $&
+  }egx
+' | awk -F ';' '{print($7)}' | sort -u | tr -d '"' | tr ',' ' ')
 
 echo $users
+
+# Llaves ssh publicas
+keys=$(tail -n +2 ASUL\ 25-1\ Datos\ debian\ -\ Respuestas\ de\ formulario\ 1\ -\ MODIFICADO.csv| perl -pe '
+  s{
+    (?:^|,)(?=(?:(?:[^"]*"){2})*[^"]*$) 
+  }{
+    $& eq "," ? ";" : $&
+  }egx
+' | awk -F ';' '{print($8)}' | sort -u | tr -d '"' | tr ',' ' ')
+
+echo $keys
+
+
+# Verificar computadoras que estan activas
+file="activas.txt"
+
+# Limpiar archivo
+> "$file"
+
+echo ""
+echo "--- Computadoras activas ---" >> "$file"
+actives=()
+
+for ip in $ip4s; do
+    if ping -c 1 -W 1 "$ip" > /dev/null 2>&1; then
+	echo "Activa: $ip" 
+	echo "$ip" >> "$file"
+        actives+=("$ip")
+    else 
+	echo "No activa: $ip"
+    fi
+done
